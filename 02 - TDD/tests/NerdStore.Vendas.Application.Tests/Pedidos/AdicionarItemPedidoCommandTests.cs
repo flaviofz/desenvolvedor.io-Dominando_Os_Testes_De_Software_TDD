6a37c2f1c10cd43dac905d92582ef47cd5a3d0ca
flavio.zavarise <flaviofz@gmail.com>
2022-01-18 07:06:02 -0300

    Pedido - Adicionar Item Command

---
+++ 02 - TDD/tests/NerdStore.Vendas.Application.Tests/Pedidos/AdicionarItemPedidoCommandTests.cs
@@ -0,0 +1,73 @@
+﻿using NerdStore.Vendas.Application.Commands;
+using NerdStore.Vendas.Domain;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace NerdStore.Vendas.Application.Tests.Pedidos
+{
+    public class AdicionarItemPedidoCommandTests
+    {
+        [Fact(DisplayName = "Adicionar item pedido válido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void AdicionarItemPedidoCommand_ComandoEstaValido_DevePassarNaValidacao()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "Produto Teste",
+                2,
+                100);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact(DisplayName = "Adicionar item pedido inválido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void AdicionarItemPedidoCommand_ComandoEstaInvalido_NaoDevePassarNaValidacao()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(
+                Guid.Empty,
+                Guid.Empty,
+                "",
+                0,
+                0);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.Contains(AdicionarItemPedidoValidation.IdClienteErroMsg, pedidoCommand.ValidationResult.Errors.Select(x => x.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidation.IdProdutoErroMsg, pedidoCommand.ValidationResult.Errors.Select(x => x.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidation.NomeErroMsg, pedidoCommand.ValidationResult.Errors.Select(x => x.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidation.QtdMinErroMsg, pedidoCommand.ValidationResult.Errors.Select(x => x.ErrorMessage));
+            Assert.Contains(AdicionarItemPedidoValidation.ValorErroMsg, pedidoCommand.ValidationResult.Errors.Select(x => x.ErrorMessage));
+        }
+
+        [Fact(DisplayName = "Adicionar item command unidades acima do permitido")]
+        [Trait("Categoria", "Vendas - Pedido Commands")]
+        public void AdicionarItemPedidoCommand_QuantidadeUnidadesSuperiorAoPermitido_NaoDevePassarNaValidacao()
+        {
+            // Arrange
+            var pedidoCommand = new AdicionarItemPedidoCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                "Produto Teste",
+                Pedido.MAX_UNIDADES_ITEM + 1,
+                100);
+
+            // Act
+            var result = pedidoCommand.EhValido();
+
+            // Assert
+            Assert.False(result); Assert.Contains(AdicionarItemPedidoValidation.QtdMaxErroMsg, pedidoCommand.ValidationResult.Errors.Select(x => x.ErrorMessage));
+        }
+    }
+}
